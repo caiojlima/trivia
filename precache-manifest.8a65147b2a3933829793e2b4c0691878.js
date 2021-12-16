b26fab9dbfdea8748ba98722a891358eb092f3fc
Caio Lima <caiojlimah@gmail.com>
2021-12-16 14:51:36 -0300

    Updates

---
+++ precache-manifest.8a65147b2a3933829793e2b4c0691878.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8bcc6709acdeb67046718078a4a16cd7",
+    "url": "/trivia/index.html"
+  },
+  {
+    "revision": "2fea7dc6c79e4b3ec256",
+    "url": "/trivia/static/css/main.1cce04d8.chunk.css"
+  },
+  {
+    "revision": "f0813d43555d636b3ee6",
+    "url": "/trivia/static/js/2.176422ab.chunk.js"
+  },
+  {
+    "revision": "a6b319ff851a22246b4802a2f165d1df",
+    "url": "/trivia/static/js/2.176422ab.chunk.js.LICENSE"
+  },
+  {
+    "revision": "2fea7dc6c79e4b3ec256",
+    "url": "/trivia/static/js/main.75fa7854.chunk.js"
+  },
+  {
+    "revision": "23dbb679c736924e4b1a",
+    "url": "/trivia/static/js/runtime-main.6e763e45.js"
+  },
+  {
+    "revision": "5e2c9f337ccce00ff99dd9e5c74c80eb",
+    "url": "/trivia/static/media/button-play.5e2c9f33.svg"
+  },
+  {
+    "revision": "5eb07356e29111c5641ee5cfd0b6f483",
+    "url": "/trivia/static/media/celular.5eb07356.svg"
+  },
+  {
+    "revision": "c2e8b0701ba9fdd04ff540600e131ff0",
+    "url": "/trivia/static/media/feedback.c2e8b070.svg"
+  },
+  {
+    "revision": "55e39bebb3b5f8e9591081306ddf810d",
+    "url": "/trivia/static/media/galaxy.55e39beb.svg"
+  },
+  {
+    "revision": "eca1efa4881c418eb31fa339068caefc",
+    "url": "/trivia/static/media/ranking.eca1efa4.svg"
+  },
+  {
+    "revision": "8ca856ee37b4d01e49472f6656f0ed6e",
+    "url": "/trivia/static/media/tablet.8ca856ee.svg"
+  },
+  {
+    "revision": "e7579ad87959e8dffa9107d51ff4adb6",
+    "url": "/trivia/static/media/trybe.e7579ad8.svg"
+  }
+]);
